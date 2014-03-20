f65c48fc3c166bea29948c4be176db863b50ff77
Casey Morton <caseyjmorton@gmail.com>
2014-03-20 14:49:15 -0400

    Configuration for In-memory database for testing

--- test/scala/DatabaseServiceActorSpec.scala
+++ test/scala/DatabaseServiceActorSpec.scala
@@ -3,35 +3,40 @@ package scala
 import akka.actor.{Props, ActorSystem}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
-import play.api.test._
 import play.api.test.Helpers._
-import play.libs.Akka
-import actor.{CreateCrawlRequest, DatabaseServiceActor}
-import models.database.{PageFetchRequest, CrawlRequest}
+import actor._
+import actor.CreateCrawlRequest
+import scala.util.Success
+import models.database.PageFetchRequest
+import models.database.CrawlRequest
+import play.api.test.FakeApplication
 
-class DatabaseServiceActorSpec (_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with BeforeAndAfterAll {
-  /*
-  val app = new FakeApplication(additionalConfiguration = inMemoryDatabase())
-  running(fakeApp = app) {
+class DatabaseServiceActorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with BeforeAndAfterAll {
 
-    "A DatabaseServiceActor" must {
-      val databaseActor = Akka.system.actorOf(Props(classOf[DatabaseServiceActor]))
+  def this() = this(ActorSystem("DatabaseServiceActorSpec"))
 
-      val cr = CrawlRequest(None, "http://www.google.com", 1, false)
-      val crWithId = cr.copy(id = Some(1))
-      val pfr = PageFetchRequest(None, crWithId.id, crWithId.origin, None, None, crWithId.initialRecursionLevel, crWithId.includeExternalLinks)
-      val pfrWithId = pfr.copy(id = Some(1))
-      val pfrWithIdAndContent = pfrWithId.copy(content = Some("<html />"), statusCode = Some(200))
+  "A DatabaseServiceActor" must {
 
-      "create a new CR" in {
-        databaseActor ! CreateCrawlRequest(cr)
-        expectMsg(crWithId)
+    "create a new CR" in {
+      running(FakeApplication()) {
+        val databaseActor = system.actorOf(Props(classOf[DatabaseServiceActor]))
+
+        val cr = CrawlRequest(None, "http://www.google.com", 1, false)
+        val crWithId = cr.copy(id = Some(1))
+        val pfr = PageFetchRequest(None, crWithId.id, crWithId.origin, None, None, crWithId.initialRecursionLevel, crWithId.includeExternalLinks)
+        val pfrWithId = pfr.copy(id = Some(1))
+        val pfrWithIdAndContent = pfrWithId.copy(content = Some("<html />"), statusCode = Some(200))
+
+        databaseActor ! CreatePageFetchRequest(pfr)
+        expectMsg(CreatedPageFetchRequest(Success(pfrWithId)))
+        databaseActor ! UpdatePageFetchRequests(row => row.id === pfrWithId.id, row => pfrWithIdAndContent)
+        expectMsg(UpdatedPageFetchRequests(Success(List(pfrWithIdAndContent))))
       }
     }
   }
 
+
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
-  */
 }
